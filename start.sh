63a401105c757d2a5e50a206935aa3f9644ac588
m-umar-j <shikaslad199@gmail.com>
2025-06-24 18:24:15 +0530

    Corrected shell command

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-cd /app && uvicorn backend.main:app --host 0.0.0.0 --port 8000 & &
+cd /app && uvicorn backend.main:app --host 0.0.0.0 --port 8000 &
 
 
 cd /app && streamlit run frontend/app.py --server.port=7860 --server.address=0.0.0.0
