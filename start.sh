93836c3387ac775da0c3bc1300ae30974faf04bc
m-umar-j <shikaslad199@gmail.com>
2025-06-24 18:54:43 +0530

    minor fixes

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@
 cd /app && uvicorn backend.main:app --host 0.0.0.0 --port 8000 &
 
 
-cd /app && streamlit run frontend/app.py --server.port=7860 --server.address=0.0.0.0 --browser.gatherUsageStats=false
+cd /app && streamlit run frontend/app.py --server.port=7860 --server.address=0.0.0.0 --browser.gatherUsageStats=false --server.enableXsrfProtection=false
 
