12d129f1c604fddfa0c9f6870c40a30d7240336a
m-umar-j <shikaslad199@gmail.com>
2025-06-24 18:22:06 +0530

    Updated shell commands

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 
-cd /app/backend && uvicorn main:app --host 0.0.0.0 --port 8000 &
+cd /app && uvicorn backend.main:app --host 0.0.0.0 --port 8000 & &
 
 
-cd /app/frontend && streamlit run app.py --server.port=7860 --server.address=0.0.0.0
+cd /app && streamlit run frontend/app.py --server.port=7860 --server.address=0.0.0.0
 
